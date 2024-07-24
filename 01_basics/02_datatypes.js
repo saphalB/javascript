0314b0569487fb3f528ca7e6259f9f52c03e56b9
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-07-24 05:38:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+        "use strict"; //treat all JS code as a newer version 
+        
+       // alert(3 + 3) //results an error since we are using nodejs, not a browser
+
+
+       let name ="Jack"
+       let age = 18
+       let isLoggedIn = false
+       let state;
+
+        // number => 2 to the power 53
+        // bigint
+        // string => " "
+        // boolean => true/false
+        // null => standalone value
+        // undefined => example=let state;
+        // symbol => unique
+
+        //object
+
+        console.log(typeof null)  //the type is object(output)
+        console.log(typeof undefined) //the type is undefined(output)
