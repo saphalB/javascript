97f6b7fd6370ffc7299ba5bedace0dbd20f2565c
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-07-24 07:13:36 +0000

    operations and operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,37 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//****************************************** Operations ************************************************* *//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2); //exponential (2^2)
+
+let str1 = "hello"
+let str2 = " jack"
+str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2"); //output - 12
+// console.log("1" + 2); //output - 12
+// console.log("1" + 2 + 2); //output - 122
+// console.log(1+ 2 + "2"); //output - 32
+
+//console.log(+true); //output - 1
+//console.log(true+); //output - error
+//console.log(+""); //output -0
+
+
+
+
+
