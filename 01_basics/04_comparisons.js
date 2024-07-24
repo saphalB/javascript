bae411568692ef3e123d727b8b3334e40bfe1940
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-07-24 07:54:12 +0000

    comaprisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,16 @@
+// console.log(2>=1)
+// console.log(2!=1)
+// console.log(2==1)  
+// console.log(2<=1)
+// console.log(2>1)
+//all these outputs to boolean value that is either true or false
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+//comparison opeartor (>= or > and so on) converts null to a number trearting it as zero(0).
+
+console.log("2" === 2); // strict check also checks for data types unlike == //
