4694528e26e0776be27008b9a9d8bcdbd42db77f
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-07-24 12:13:05 +0000

    datatypes summary

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+// Primitive datatypes : Call by value //
+// 7 primitive datatypes are : String, Number, Null, Boolean, Undefined, Symbol & BigInt
+
+const id = Symbol('123')
+const id2 = Symbol('123')
+console.log(id === id2) 
+
+
+// Non-Primitive datatypes : call by reference or reference type datatypes.
+// types : arrays,objects & functions.
+
+const heroes = ["Spiderman", "Superman","Hulk"]
+{
+    name: "Jack"  
+    age = 20
+}
+
+console.log(typeof age)
+
