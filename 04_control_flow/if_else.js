0bb8ab7f1e5ff9bd0117650af18311aa2e7a5ed6
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-07-27 13:08:29 +0000

    control flow

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,60 @@
+// if else statement
+
+
+//    const temperature = 40
+
+// if( temperature === 42){
+//   console.log("Temperature is higher than 40");
+
+// }
+// else{
+//     console.log("tempertature is less than 42");
+// }
+
+
+
+// const score = 100
+
+// if (score > 50) {
+
+//     const power = "fly"
+    
+//     console.log(`user can ${power}`);
+    
+// }
+//     console.log(`user can ${power}`);    // global scope problem that's why causes error
+
+
+// const balance = 1000
+// if (balance < 2000)    console.log("low balance");   // if statement shorthand
+
+// const balance = 1000
+// if (balance < 2000)    console.log("low balance"), console.log("insufficient balance");      // not a good practice
+
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("greater than 500");
+// }
+// else if (balance > 750) {
+//     console.log("greater than 750");
+// }
+// else { 
+//     console.log("greater than 999");
+// }
+
+
+
+// const isLoggedIn = true
+// const debitcard = true
+// const loggedInFromEmail = true
+// const loggedInFromGmail = false
+
+// if(isLoggedIn && debitcard ){
+//     console.log("Allow login");
+// }
+
+// if(loggedInFromEmail || loggedInFromGmail){
+//     console.log("Allow");
+// }
---
+++ 04_control_flow/truth_or_false.js
@@ -0,0 +1,49 @@
+
+//  falsy values : false, 0, -0, BigInt 0n, "", null, undefined, NaN  
+
+// truthy values : "0", 'false', " ", array[], object{}, function(){}
+
+// const userEmail = []
+
+// if(userEmail){
+//     console.log("Got User Email");
+// }
+// else{
+//     console.log("nOt an email");
+// }
+
+// if(userEmail.length === 0){
+//   console.log("Empty Array");
+// }
+
+
+
+// const emptyObj ={}
+
+// if(Object.keys(emptyObj).length === 0){    // can use either keys or entries or values after Object.
+//  console.log("Object is empty");
+// }
+
+
+
+// NULL COALESCING OPERATOR  (??) : for null and undefined
+
+let val1; 
+// val1 = 5 ?? 10                         // output:5
+// val1 = 10 ?? 5                         // output:10
+//  val1 = null ?? 10                     // output:10
+// val1 = undefined ?? 5                 // output:5
+//  val1 = null ?? 10 ?? 15              // output:5
+// console.log(val1);
+
+
+
+// TERNARY Operator
+
+// condition ? true : false                         // syntax of ternary operator
+
+//  const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("Price is less than 80") : console.log("greater than 80");
+
+
+
