a1fc921de2b9f0ee6698446c1341f5646066e17f
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-07-28 14:32:46 +0000

    do and while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,43 @@
+
+
+let index = 0
+while (index < 10 ) {
+// console.log(`index is ${index}`);
+    index = index + 3
+}
+
+
+
+let myArray =[ "flash" , "batman" , "superman"]
+
+let arr = 0
+ while(arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr ++
+ }
+
+
+
+
+
+
+let score = 1
+ do {
+    // console.log(`score is ${score}`);
+     score ++
+ } while (score < 5);
+
+
+
+
+
+
+
+
+
+ // special case 
+ let number = 7
+ do {
+    console.log(`score is ${number}`);             // output:  (score is 7) 
+     number ++
+ } while (number < 5);
