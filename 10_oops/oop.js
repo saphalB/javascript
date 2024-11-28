50d4df4ff3bfa6fb37947eb59a7c52b5690fe704
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-11-28 08:37:48 +0000

    constructor and object literals

---
+++ 10_oops/oop.js
@@ -0,0 +1,44 @@
+// object literal //
+
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database"); 
+       // console.log(`Username: ${username}`)               // throws error 
+      // console.log(`Username: ${this.username}`)            // prints username
+       console.log(this);                                  // prints all the details of current context
+       
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+
+
+
+// construction functions //
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+     this.greeting = function(){
+         console.log(`Welcome ${this.username}`);
+
+    
+
+    return this;                                      // no need to write 
+}
+}
+
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
